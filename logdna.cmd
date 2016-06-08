dea06be142f91a735b973dcbf0652c86da2cd1f9
respectus <muazs786@gmail.com>
2016-06-08 15:04:47 -0700

    lolnothelpful

--- logdna.cmd
+++ logdna.cmd
@@ -1,8 +1,12 @@
-echo. 2>EmptyFile.js
-MKDIR c:example1
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%chocolateybin
-choco install logdna-agent -y
-logdna-agent -k d8e14421399a44a9a35dfc49c7f5f0aa # this is your unique Agent API Key
-:: by default the agent monitors %ALLUSERSPROFILE%logs
-logdna-agent -d C:LogFiles
-nssm start logdna-agent
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%chocolateybin\
+choco install logdna-agent -y\
+logdna-agent -k d8e14421399a44a9a35dfc49c7f5f0aa # this is your unique Agent API Key\
+:: by default the agent monitors %ALLUSERSPROFILE%logs\
+logdna-agent -d C:\\LogFiles\
+nssm start logdna-agent}
