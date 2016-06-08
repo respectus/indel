51e2764d797625d14eb02e7e7aefabf286290ed4
respectus <muazs786@gmail.com>
2016-06-07 17:53:44 -0700

    bat to cmd

--- logdna_batch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco install logdna-agent -y
-logdna-agent -k d8e14421399a44a9a35dfc49c7f5f0aa # this is your unique Agent API Key
-:: by default the agent monitors %ALLUSERSPROFILE%\logs
-logdna-agent -d C:\LogFiles
-nssm start logdna-agent
