607881540f78f580b4165f4eac670c886cd806f2
respectus <muazs786@gmail.com>
2016-06-09 10:46:29 -0700

    trying to manually restart

--- logdna.cmd
+++ logdna.cmd
@@ -4,7 +4,8 @@
 \margl1440\margr1440\vieww10800\viewh8400\viewkind0
 \pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs20 \cf0 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%chocolateybin\
+\f0\fs20 \cf0 echo. 2>EmptyFile.js\
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%chocolateybin\
 choco install logdna-agent -y\
 logdna-agent -k d8e14421399a44a9a35dfc49c7f5f0aa # this is your unique Agent API Key\
 :: by default the agent monitors %ALLUSERSPROFILE%logs\
